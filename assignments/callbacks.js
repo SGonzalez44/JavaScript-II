86756676c3b039a1d15b8475c0e60d9f22c12932
Salvador <salgonzolanda@gmail.com>
2019-07-23 16:12:44 -0700

    array done

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -8,6 +8,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
+    cb(arr[0]);
   }
 
   // Potential Solution:
@@ -27,25 +28,49 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, function(length){
+console.log(length)
+})
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+ return cb(arr[arr.length-1]);
+ console.log(cb)
 }
+
+
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let z = x+y;
+ return cb(z);
 }
+
+
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let z = x * y;
+  return cb(z);
 }
+
+
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(item))
+  
 }
 
+
+
+contains('Pencil', items, console.log)
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
